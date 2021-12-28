c7b1a6bb873e1ae8d01f1737ff6103d8e822da2f
hansolc <hsc080808@naver.com>
2021-12-28 19:56:53 +0900

    Router init setting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDom.render(<App/>, document.getElementById('root'))
+// 1. 최상위 노드에 BrowserRouter 태그를 추가한다. App내의 어디서든
+// React Router을 사용할 수 있도록 설정
+ReactDom.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
+    document.getElementById('root')
+)
